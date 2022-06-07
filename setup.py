e6d08191bee75158858cd3c04a516a77dabd1e80
meta-tabchen <2808581543@qq.com>
2022-06-07 18:36:23 +0800

    update & add build

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="pykt-toolkit",
-    version="0.0.2",
+    version="0.0.3",
     author="pykt-team",
     author_email="pykt.team@gmail.com",
     description="pyKT: A Python Library to Benchmark Deep Learning based Knowledge Tracing Models",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=['pykt'],
+    packages=find_packages(),
     python_requires=">=3.5",
     include_package_data=True,
     install_requires=['pandas'],
