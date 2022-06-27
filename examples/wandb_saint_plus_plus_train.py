23a068c7d5cd70dc8dae1a120616b019dbfa71fb
meta-tabchen <2808581543@qq.com>
2022-06-27 17:16:26 +0800

    add iekt

---
+++ examples/wandb_saint_plus_plus_train.py
@@ -0,0 +1,25 @@
+import argparse
+from wandb_train import main
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name", type=str, default="assist2015")
+    parser.add_argument("--model_name", type=str, default="saint++")
+    parser.add_argument("--emb_type", type=str, default="qid")
+    parser.add_argument("--save_dir", type=str, default="saved_model")
+    # parser.add_argument("--learning_rate", type=float, default=1e-5)
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--fold", type=int, default=0)
+    parser.add_argument("--dropout", type=float, default=0.2)
+    
+    parser.add_argument("--emb_size", type=int, default=256)
+    parser.add_argument("--learning_rate", type=float, default=1e-3)
+    parser.add_argument("--num_attn_heads", type=int, default=8)
+    parser.add_argument("--n_blocks", type=int, default=4)
+    parser.add_argument("--use_wandb", type=int, default=1)
+    parser.add_argument("--add_uuid", type=int, default=1)
+   
+    args = parser.parse_args()
+
+    params = vars(args)
+    main(params)
