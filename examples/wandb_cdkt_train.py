273da214feecfc4bd1effe5f074a39ade8d32cae
Liu-lqq <liuqiongqiong91@163.com>
2022-07-04 15:17:48 +0800

    add cdkt

--- examples/wandb_cdkt_train.py
+++ examples/wandb_cdkt_train.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     parser.add_argument("--model_name", type=str, default="cdkt")
     parser.add_argument("--emb_type", type=str, default="qid")
     parser.add_argument("--save_dir", type=str, default="saved_model")
-    # parser.add_argument("--learning_rate", type=float, default=1e-5)
     parser.add_argument("--seed", type=int, default=3407)
     parser.add_argument("--fold", type=int, default=0)
     parser.add_argument("--dropout", type=float, default=0.05)
