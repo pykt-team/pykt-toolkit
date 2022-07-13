7a4561c3535ee84cb1231a60bead4a11f93ac54e
meta-tabchen <2808581543@qq.com>
2022-07-13 15:48:11 +0800

    update

--- pykt/models/dkt_que.py
+++ pykt/models/dkt_que.py
@@ -137,7 +137,6 @@ class DKTQueNet(nn.Module):
             h = torch.cat([h,h_ccs],dim=-1)#add the last hidden state of kcs lstm to the last hidden state of lstm
             # print(f"h.shape is {h.shape}")
         
-
         if self.predict_next:
             if self.emb_type in ['iekt']:
                 seq_len = q.shape[-1]
@@ -193,17 +192,17 @@ class DKTQue(QueBaseModel):
         self.eval_result = {}
        
     def get_merge_loss(self,loss_question,loss_concept,loss_question_concept):
-        if self.model.loss_mode=="c":
+        if self.model.loss_mode in ["c","c_fr"]:
             loss = loss_concept
         elif self.model.loss_mode in ["c_cc","c_ccs"]:
             loss = (loss_concept+self.model.loss_question_concept_lambda*loss_question_concept)/(1+self.model.loss_question_concept_lambda)
-        elif self.model.loss_mode=="q":
+        elif self.model.loss_mode in ["q","q_fr"]:
             loss = loss_question
         elif self.model.loss_mode in ["q_cc","q_ccs"]:#concept classifier
             loss = (loss_question+loss_question_concept)/2
         elif self.model.loss_mode in ["cc"]:#concept classifier
             loss = loss_question_concept
-        elif self.model.loss_mode=="qc":
+        elif self.model.loss_mode in ["qc","qc_fr"]:
             loss = (loss_question+loss_concept*self.model.qc_loss_mode_lambda)/(1+self.model.qc_loss_mode_lambda)
         elif self.model.loss_mode in ["qc_cc","qc_ccs"]:#concept classifier
             loss = (loss_question+loss_concept+loss_question_concept)/3
@@ -216,19 +215,77 @@ class DKTQue(QueBaseModel):
             alpha_kc = (acc_kt+c_dyn_b)/(acc_kt+acc_kc+c_dyn_a+c_dyn_b)
             print(f"acc_kt={acc_kt},acc_kc={acc_kc},alpha_kt={alpha_kt},alpha_kc={alpha_kc},c_dyn_a={c_dyn_a},c_dyn_b={c_dyn_b}")
             loss = alpha_kt*loss_concept + alpha_kc*loss_question_concept
+        
         return loss
 
+    def get_avg_fusion_concepts(self,y_concept,cshft):
+        """获取知识点 fusion 的预测结果
+        """
+        concept_mask = torch.where(cshft.long()==-1,False,True)
+        concept_index = F.one_hot(torch.where(cshft!=-1,cshft,0),self.model.num_c)
+        concept_sum = (y_concept.unsqueeze(2).repeat(1,1,4,1)*concept_index).sum(-1)
+        concept_sum = concept_sum*concept_mask#remove mask
+        y_concept = concept_sum.sum(-1)/torch.where(concept_mask.sum(-1)!=0,concept_mask.sum(-1),1)
+        return y_concept
+
+
     def train_one_step(self,data,process=True,return_all=False):
-        y,y_question,y_concept,y_question_concepts,y_qc_predict,qc_target,data_new = self.predict_one_step(data,return_details=True,process=process)
-        # print(f"y_question_concepts shape is {y_question_concepts.shape}")
-        loss_question = self.get_loss(y_question,data_new['rshft'],data_new['sm'])#get loss
-        loss_concept = self.get_loss(y_concept,data_new['rshft'],data_new['sm'])#get loss
         
-        #知识点分类当作多分类
-        loss_func = Loss(self.model.other_config.get("loss_type","ce"),
-                        epsilon=self.model.other_config.get("epsilon",1.0),
-                        gamma=self.model.other_config.get("gamma",2)).get_loss
-        loss_question_concept = loss_func(y_qc_predict,qc_target)
+        if "fr" in self.model.loss_mode:#only for predict all
+            y_question_raw,y_concept_raw,y_question_concepts_raw,data_new = self.predict_one_step(data,return_details=True,process=process,return_raw=True)
+            seq_len = data_new['qshft'].shape[1]
+            loss_question = 0
+            loss_concept = 0
+            num_inter = 0
+            for i in range(seq_len):
+                #new mask
+                fr_window = self.model.other_config.get("fr_window",1)
+                sm_step = data_new['sm'][:,i:i+fr_window]
+                num_inter_step = sm_step.sum()
+                if num_inter_step==0:
+                    break
+                rshft_step = data_new['rshft'][:,i:i+fr_window]
+                cshft_step = data_new['cshft'][:,i:i+fr_window]
+                qshft_step = data_new['qshft'][:,i:i+fr_window]
+
+                valid_seq_len = min(fr_window,rshft_step.shape[1])
+                num_inter+=num_inter_step
+                #new y_concept
+                current_step_concept_raw = y_concept_raw[:,i].unsqueeze(1)
+                # print(f"current_step_concept_raw shape is {current_step_concept_raw.shape}")
+                current_concept_expand = current_step_concept_raw.repeat(1,valid_seq_len,1)
+                # current_concept_expand = current_step_concept_raw.repeat(-1,valid_seq_len,-1)
+                y_concept_step = self.get_avg_fusion_concepts(current_concept_expand,cshft_step)
+      
+                loss_concept_step =self.get_loss(y_concept_step,rshft_step,sm_step)
+                loss_concept = loss_concept+loss_concept_step*num_inter_step
+            
+               
+                #new y_question
+                current_step_question_raw = y_question_raw[:,i].unsqueeze(1)
+                current_question_expand = current_step_question_raw.repeat(1,valid_seq_len,1)
+                # current_question_expand = current_step_question_raw.repeat(-1,valid_seq_len,-1)
+                # print(f"current_question_expand shape is {current_question_expand.shape}")
+                y_question_step = (current_question_expand * F.one_hot(qshft_step.long(), self.model.num_q)).sum(-1)
+                loss_question_step = self.get_loss(y_question_step,rshft_step,sm_step)#question level loss
+                loss_question = loss_question + loss_question_step*num_inter_step
+               
+
+            loss_question = loss_question/num_inter
+            loss_concept = loss_concept/num_inter
+            loss_question_concept = 0
+            y_question = None
+        else:            
+            y,y_question,y_concept,y_question_concepts,y_qc_predict,qc_target,data_new = self.predict_one_step(data,return_details=True,process=process)
+            # print(f"y_question_concepts shape is {y_question_concepts.shape}")
+            loss_question = self.get_loss(y_question,data_new['rshft'],data_new['sm'])#question level loss
+            loss_concept = self.get_loss(y_concept,data_new['rshft'],data_new['sm'])#kc level loss
+            
+            #知识点分类当作多分类
+            loss_func = Loss(self.model.other_config.get("loss_type","ce"),
+                            epsilon=self.model.other_config.get("epsilon",1.0),
+                            gamma=self.model.other_config.get("gamma",2)).get_loss
+            loss_question_concept = loss_func(y_qc_predict,qc_target)#question concept level loss
 
         print(f"loss_question is {loss_question:.4f},loss_concept is {loss_concept:.4f},loss_question_concept is {loss_question_concept:.4f}")
         
@@ -276,35 +333,32 @@ class DKTQue(QueBaseModel):
         self.eval_result = eval_result
         return eval_result
         
-
-    def predict_one_step(self,data,return_details=False,process=True):
-        data_new = self.batch_to_device(data,process=process)
-        if self.model.emb_type in ["iekt"]:
-            y_question,y_concept,y_question_concepts = self.model(data_new['cq'].long(),data_new['cc'],data_new['cr'].long(),data=data_new)
-        else:
-            y_question,y_concept,y_question_concepts = self.model(data_new['q'].long(),data_new['c'],data_new['r'].long(),data=data_new)
-        # print(y_question.shape,y_concept.shape)
-        if self.model.predict_next:
-            y_question = y_question.squeeze(-1)
-        else:
-            y_question = (y_question * F.one_hot(data_new['qshft'].long(), self.model.num_q)).sum(-1)
-
-        #get y_concept
-        # print(y_question.shape,y_concept.shape)
-        concept_mask = torch.where(data_new['cshft'].long()==-1,False,True)
-        concept_index = F.one_hot(torch.where(data_new['cshft']!=-1,data_new['cshft'],0),self.model.num_c)
-        concept_sum = (y_concept.unsqueeze(2).repeat(1,1,4,1)*concept_index).sum(-1)
-        concept_sum = concept_sum*concept_mask
-        y_concept = concept_sum.sum(-1)/torch.where(concept_mask.sum(-1)!=0,concept_mask.sum(-1),1)
-
-
-
+    def get_qc_predict_result(self,y_question_concepts,data_new):
         #知识点分类当作多分类
         concept_target = data_new['cshft'][:,:,0].flatten(0,1)
         y_question_concepts = y_question_concepts.flatten(0,1)
         qc_target = concept_target[concept_target!=-1]
         y_qc_predict = y_question_concepts[concept_target!=-1,:]
+        return qc_target,y_qc_predict
 
+    def predict_one_step(self,data,return_details=False,process=True,return_raw=False):
+        data_new = self.batch_to_device(data,process=process)
+        
+        if self.model.emb_type in ["iekt"]:
+            y_question,y_concept,y_question_concepts = self.model(data_new['cq'].long(),data_new['cc'],data_new['cr'].long(),data=data_new)
+        else:
+            y_question,y_concept,y_question_concepts = self.model(data_new['q'].long(),data_new['c'],data_new['r'].long(),data=data_new)
+        # print(y_question.shape,y_concept.shape)
+        if return_raw:#return raw probability
+            return y_question,y_concept,y_question_concepts,data_new
+        else:
+            if self.model.predict_next:
+                y_question = y_question.squeeze(-1)
+            else:
+                y_question = (y_question * F.one_hot(data_new['qshft'].long(), self.model.num_q)).sum(-1)
+
+            y_concept = self.get_avg_fusion_concepts(y_concept,data_new['cshft'])
+            qc_target,y_qc_predict = self.get_qc_predict_result(y_question_concepts,data_new)
 
         if self.model.predict_mode=="c":
             y = y_concept
