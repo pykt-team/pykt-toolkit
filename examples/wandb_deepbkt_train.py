0ce37d0a0c19c9c992a60019fca1e2c03a6958d8
Liu-lqq <liuqiongqiong91@163.com>
2022-07-23 12:46:19 +0800

    three layers

--- examples/wandb_deepbkt_train.py
+++ examples/wandb_deepbkt_train.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
     # parser.add_argument("--learning_rate", type=float, default=1e-5)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--fold", type=int, default=0)
-    parser.add_argument("--dropout", type=float, default=0.05)
+    parser.add_argument("--dropout", type=float, default=0.2)
     
     parser.add_argument("--d_model", type=int, default=256)
-    parser.add_argument("--d_ff", type=int, default=256)
+    parser.add_argument("--d_ff", type=int, default=512)
     parser.add_argument("--num_attn_heads", type=int, default=8)
-    parser.add_argument("--n_blocks", type=int, default=1)
-    parser.add_argument("--learning_rate", type=float, default=1e-5)
+    parser.add_argument("--n_blocks", type=int, default=4)
+    parser.add_argument("--learning_rate", type=float, default=1e-4)
 
     parser.add_argument("--use_wandb", type=int, default=1)
     parser.add_argument("--add_uuid", type=int, default=1)
