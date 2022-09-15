e99ee8da691b6d0f43882556c59939faa86bbb01
meta-tabchen <2808581543@qq.com>
2022-09-15 17:21:39 +0800

    update

--- examples/dataprocess.sh
+++ examples/dataprocess.sh
@@ -4,3 +4,4 @@ nohup python -u data_preprocess.py -d algebra2005 >algebra2005.log&
 nohup python -u data_preprocess.py -d nips_task34 >nips_task34.log&
 nohup python -u data_preprocess.py -d statics2011 >statics2011.log&
 nohup python -u data_preprocess.py -d junyi2015 >junyi2015.log&
+nohup python -u data_preprocess.py -d bridge2algebra2006 >bridge2algebra2006.log&
