f54d37e7a821f4c37cc6a807350ca7dbdc8a2db2
tabchen <liuqiongqiong91@163.com>
2022-09-26 17:57:46 +0800

    fix kqn_fusion

--- pykt/models/evaluate_model.py
+++ pykt/models/evaluate_model.py
@@ -179,11 +179,11 @@ def effective_fusion(df, model, model_name, fusion_type):
 
     curhs, curr = [[], []], []
     dcur = {"late_trues": [], "qidxs": [], "questions": [], "concepts": [], "row": [], "concept_preds": []}
-    hasearly = ["dkvmn","deep_irt", "skvmn", "akt", "saint", "sakt", "hawkes", "akt_vector", "akt_norasch", "akt_mono", "akt_attn", "aktattn_pos", "aktmono_pos", "akt_raschx", "akt_raschy", "aktvec_raschx"]
+    hasearly = ["dkvmn","deep_irt", "skvmn", "kqn", "akt", "saint", "sakt", "hawkes", "akt_vector", "akt_norasch", "akt_mono", "akt_attn", "aktattn_pos", "aktmono_pos", "akt_raschx", "akt_raschy", "aktvec_raschx", "lpkt"]
     for ui in df:
         # 一题一题处理
         curdf = ui[1]
-        if model_name in hasearly:
+        if model_name in hasearly and model_name != "kqn" and model_name != "lpkt":
             curhs[0].append(curdf["hidden"].mean().astype(float))
         elif model_name == "kqn":
             curhs[0].append(curdf["ek"].mean().astype(float))
@@ -246,6 +246,8 @@ def group_fusion(dmerge, model, model_name, fusion_type, fout):
         elif model_name == "lpkt":
             df["h"] = [np.array(a) for a in hs[0][bz].cpu().tolist()]
             df["e_data"] = [np.array(a) for a in hs[1][bz].cpu().tolist()]
+            df["h"] = [np.array(a) for a in hs[0][bz].cpu().tolist()]
+            df["e_data"] = [np.array(a) for a in hs[1][bz].cpu().tolist()]
 
         df = df[df["select"] != 0]
         alldfs.append(df)
@@ -270,7 +272,7 @@ def group_fusion(dmerge, model, model_name, fusion_type, fout):
             if model_name in hasearly and model_name != "kqn" and model_name != "lpkt":
                 drest[key] = [dmerge[key][0][rest_start:]]
             elif model_name == "kqn" or model_name == "lpkt":
-                drest[key] = [dmerge[key][0][rest_start:], dmerge[key][1][rest_start:]]              
+                drest[key] = [dmerge[key][0][rest_start:], dmerge[key][1][rest_start:]]             
         else:
             drest[key] = dmerge[key][rest_start:] 
     restlen = drest["cr"].shape[0]
@@ -389,8 +391,8 @@ def evaluate_question(model, test_loader, model_name, fusion_type=["early_fusion
                 y = model(cc.long(), cq.long(), ct.long(), cr.long(), True)
                 y, h = y[:, 1:]
             elif model_name == "lpkt":
-                cit = torch.cat((dcur["itseqs"][:,0:1], dcur["shft_itseqs"]), dim=1)
-                y, h, e_data = model(cq.long(), cr.long(), cit.long(), True)
+                cit = torch.cat((dcurori["itseqs"][:,0:1], dcurori["shft_itseqs"]), dim=1)
+                y, h, e_data = model(cq.long(), cr.long(), cit.long(), at_data=None, qtest=True)
                 start_hemb = torch.tensor([-1] * (h.shape[0] * h.shape[2])).reshape(h.shape[0], 1, h.shape[2]).to(device) # add the first hidden emb
                 h = torch.cat((start_hemb, h), dim=1)
                 # e_data = torch.cat((start_hemb, e_data), dim=1)
