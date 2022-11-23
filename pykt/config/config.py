4d772f3739da476beee0329abe4ba9a4512e91f0
sonyawong <hsy1225@126.com>
2022-11-23 11:16:35 +0800

    update config

--- pykt/config/config.py
+++ pykt/config/config.py
@@ -1,4 +1,4 @@
-que_type_models = ["iekt","qdkt","qikt","qikt_ab_a+b+c","qikt_ab_a+b+c+irt","qikt_ab_a+b+irt","qikt_ab_a+c+irt","qikt_ab_a+irt","qikt_ab_b+irt"]
+que_type_models = ["iekt","qdkt","qikt"]
 
 qikt_ab_models = ["qikt_ab_a+b+c","qikt_ab_a+b+c+irt","qikt_ab_a+b+irt","qikt_ab_a+c+irt","qikt_ab_a+irt","qikt_ab_b+irt"]
 
