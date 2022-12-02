1e1e4a0cecabe62227d4d025a574e6fe40d57df3
meta-tabchen <2808581543@qq.com>
2022-12-02 12:16:35 +0800

    update lpkt

--- pykt/config/config.py
+++ pykt/config/config.py
@@ -1,4 +1,4 @@
-que_type_models = ["iekt","qdkt","qikt"]
+que_type_models = ["iekt","qdkt","qikt","lpkt"]
 
 qikt_ab_models = ["qikt_ab_a+b+c","qikt_ab_a+b+c+irt","qikt_ab_a+b+irt","qikt_ab_a+c+irt","qikt_ab_a+irt","qikt_ab_b+irt"]
 
