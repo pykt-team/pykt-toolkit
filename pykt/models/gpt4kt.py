2b38264c2db0206df1ffe9f2f180864eb180098b
sonyawong <hsy1225@126.com>
2023-06-29 17:14:17 +0800

    add pos

--- pykt/models/gpt4kt.py
+++ pykt/models/gpt4kt.py
@@ -24,7 +24,7 @@ class GPT4KT(nn.Module):
     def __init__(self, n_question, n_pid, 
             d_model, n_blocks, dropout, d_ff=256, 
             loss1=0.5, loss2=0.5, loss3=0.5, start=50, num_layers=2, nheads=4, seq_len=1024, 
-            kq_same=1, final_fc_dim=512, final_fc_dim2=256, num_attn_heads=8, separate_qa=False, l2=1e-5, emb_type="qid", emb_path="", pretrain_dim=768, cf_weight=0.3, t_weight=0.3, num_sgap=None):
+            kq_same=1, final_fc_dim=512, final_fc_dim2=256, num_attn_heads=8, separate_qa=False, l2=1e-5, emb_type="qid", emb_path="", pretrain_dim=768, cf_weight=0.3, t_weight=0.3, num_sgap=None, c0=0):
         super().__init__()
         """
         Input:
