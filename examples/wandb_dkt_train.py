5dbbf50b92a083ef6b00231d4c0fb41d905a8707
Kaveh Karbasi <kkarbasi@berkeley.edu>
2024-05-17 14:53:29 -0700

    add pretrained as a wanb param

--- examples/wandb_dkt_train.py
+++ examples/wandb_dkt_train.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--use_wandb", type=int, default=1)
     parser.add_argument("--add_uuid", type=int, default=1)
+    parser.add_argument("--pretrained_emb_path", type=str, default="")
     
     args = parser.parse_args()
 
--- pykt/models/dkt.py
+++ pykt/models/dkt.py
@@ -6,7 +6,8 @@ import torch
 from torch.nn import Module, Embedding, LSTM, Linear, Dropout
 
 class DKT(Module):
-    def __init__(self, num_c, emb_size, dropout=0.1, emb_type='qid', emb_path="", pretrain_dim=768):
+    def __init__(self, num_c, emb_size, dropout=0.1, emb_type='qid', pretrained_emb_path="", emb_path=None):
+        # emb_path is a dummy param to deal with the current data_config design. Will need to remove. Ignore for now
         super().__init__()
         self.model_name = "dkt"
         self.num_c = num_c
@@ -14,21 +15,25 @@ class DKT(Module):
         self.hidden_size = emb_size
         self.emb_type = emb_type
 
+            
         if emb_type.startswith("qid"):
-            self.interaction_emb = Embedding(self.num_c * 2, self.emb_size)
-
+            if pretrained_emb_path.endswith('.pt'):
+                emb_w = torch.load(pretrained_emb_path)
+                self.emb_size = emb_w.shape[-1]
+                self.interaction_emb = Embedding.from_pretrained(emb_w, freeze=True)
+            else:
+                self.interaction_emb = Embedding(self.num_c * 2, self.emb_size)
+                
         self.lstm_layer = LSTM(self.emb_size, self.hidden_size, batch_first=True)
         self.dropout_layer = Dropout(dropout)
         self.out_layer = Linear(self.hidden_size, self.num_c)
         
 
     def forward(self, q, r):
-        # print(f"q.shape is {q.shape}")
         emb_type = self.emb_type
         if emb_type == "qid":
             x = q + self.num_c * r
             xemb = self.interaction_emb(x)
-        # print(f"xemb.shape is {xemb.shape}")
         h, _ = self.lstm_layer(xemb)
         h = self.dropout_layer(h)
         y = self.out_layer(h)
