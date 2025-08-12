f30ac726633d5c4e1a339c4a149f2178e43e43cf
Shuyan Huang <44161671+sonyawong@users.noreply.github.com>
2025-08-13 01:07:25 +0800

    Update wandb_train.py
    rename datakt

--- examples/wandb_train.py
+++ examples/wandb_train.py
@@ -41,7 +41,7 @@ def main(params):
         train_config = config["train_config"]
         if model_name in ["dkvmn","deep_irt", "sakt", "saint","saint++", "akt", "robustkt", "folibikt", "atkt", "lpkt", "skvmn", "dimkt"]:
             train_config["batch_size"] = 64 ## because of OOM
-        if model_name in ["simplekt","stablekt", "bakt_time", "sparsekt"]:
+        if model_name in ["simplekt","stablekt", "datakt", "sparsekt"]:
             train_config["batch_size"] = 64 ## because of OOM
         if model_name in ["gkt"]:
             train_config["batch_size"] = 16 
