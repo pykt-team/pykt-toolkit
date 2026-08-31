c195b9915a0d01f77b581b3f8fc42b1ab4255664
18229048101 <17359578+zhq1252678138@user.noreply.gitee.com>
2026-08-31 11:51:13 +0800

    fix: set OPERA enhance pro epochs to 200

--- examples/wandb_dkt_enhance_pro_train.py
+++ examples/wandb_dkt_enhance_pro_train.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     parser.add_argument("--dropout", type=float, default=0.2)
     parser.add_argument("--emb_size", type=int, default=64)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
-    parser.add_argument("--num_epochs", type=int, default=1)
+    parser.add_argument("--num_epochs", type=int, default=200)
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--use_wandb", type=int, default=0)
     parser.add_argument("--add_uuid", type=int, default=0)
